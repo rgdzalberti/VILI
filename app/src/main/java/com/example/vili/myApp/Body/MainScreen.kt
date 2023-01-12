25ef8795d88dabd988f66456fa190d35c517a5be
Ricardo <rgallego@iesrafaelalberti.es>
2023-01-12 19:14:26 +0100

    early mainscreen footer

--- app/src/main/java/com/example/vili/myApp/Body/MainScreen.kt
+++ app/src/main/java/com/example/vili/myApp/Body/MainScreen.kt
@@ -1,12 +1,22 @@
 package viliApp
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -14,17 +24,57 @@ fun PreviewMainScreen(){
     mainScreen() //TODO navcontroller
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun mainScreen(){
 
     systemBarColor(color = Color(0xFF0A0A0A))
     getDeviceConfig()
 
-    Column(Modifier.fillMaxSize().background( Color(0xFF0A0A0A))) {
-        
-    }
-
-    //TODO Footer
     //TODO desplegable izq
+
+    Scaffold(bottomBar = {BottomBar()}) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(Color(0xFF0A0A0A))) {
+
+        }
+    }
     
 }
+
+@Composable
+fun BottomBar() {
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation(elevation = 10.dp) {
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Home,"")
+        },
+            label = { Text(text = "Home") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Favorite,"")
+        },
+            label = { Text(text = "Favorite") },
+            selected = (selectedIndex.value == 1),
+            onClick = {
+                selectedIndex.value = 1
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Person,"")
+        },
+            label = { Text(text = "Profile") },
+            selected = (selectedIndex.value == 2),
+            onClick = {
+                selectedIndex.value = 2
+            })
+    }
+}
+
